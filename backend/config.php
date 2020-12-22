2fe21898b10e0d684e4f5d31d69c05bccd2bd414
Alexis Gonzalez <gonzalezalexis034@gmail.com>
2020-12-22 05:47:17 -0600

    Correcciones Varias

--- backend/config.php
+++ backend/config.php
@@ -4,6 +4,13 @@
 // I left password empty because i do not set password on localhost.
 $con = mysqli_connect("localhost","root","","quillota");
 define('NUM_ITEMS_BY_PAGE', 42);
+/* cambiar el conjunto de caracteres a utf8 */
+if (!$con->set_charset("utf8")) {
+  printf("Error cargando el conjunto de caracteres utf8: %s\n", $con->error);
+  exit();
+} else {
+  #printf("Conjunto de caracteres actual: %s\n", $con->character_set_name());
+}
 #$con = mysqli_connect("localhost","tarjhgww_root","metasoft123.","tarjhgww_quillota");
 // Check connection
 if (mysqli_connect_errno())
